89b2ffb5ae2db2c0a0f34c5d6d45f415416c6d21
weiming97 <Acousticxxxi@gmail.com>
2019-03-14 16:31:35 +0800

    change 3030 to 3031

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http');
 var app = require('./config/express');
 
 http.createServer(app)
-.listen(3030, function () {
+.listen(3031, function () {
 	console.log('App simple-mock-server started');
 });
